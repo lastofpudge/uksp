84ec223ed1dc2616662caf51014b720052819509
Andrey Dudin <iluxor@yandex.ru>
2022-03-15 02:03:46 +0200

    initial

---
+++ uksp.js
@@ -0,0 +1,16 @@
+(function() {
+
+  let help = `<div class="g2-line">
+<div class="g2-line__content">
+<span class="g2-line__text show">
+  <span>Stop Russian invasion into Europe. Learn how you can help Ukraine withstand</span>
+  <a class="btn-save" href="https://helpukrainewin.org/" target="_blank">HERE</a>
+</span>
+<br>
+<span class="stand">#StandWithUkraine</span>
+</div>
+</div>`;
+
+  document.body.appendChild(help);
+
+})();
