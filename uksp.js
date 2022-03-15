ae3c1d5eecab4b1ecf7c84b092e7c82aef9414ce
Andrey Dudin <iluxor@yandex.ru>
2022-03-15 02:21:13 +0200

    initial

--- uksp.js
+++ uksp.js
@@ -1,16 +1,11 @@
-(function() {
-
-  let help = `<div class="g2-line">
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.body.innerHTML += `<div class="g2-line">
 <div class="g2-line__content">
 <span class="g2-line__text show">
   <span>Stop Russian invasion into Europe. Learn how you can help Ukraine withstand</span>
-  <a class="btn-save" href="https://helpukrainewin.org/" target="_blank">HERE</a>
+  <a class="g2-line__btn-save" href="https://helpukrainewin.org/" target="_blank">HERE</a>
 </span>
-<br>
-<span class="stand">#StandWithUkraine</span>
+<span class="g2-line__stand">#StandWithUkraine</span>
 </div>
 </div>`;
-
-  document.body.appendChild(help);
-
-})();
+});
